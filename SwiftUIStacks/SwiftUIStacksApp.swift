3f14cfd2c984ba26fe395c6435af17c8f8aa3529
Your mato <you@example.com>
2020-12-20 20:44:31 +0800

    Initial Commit

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by kittawat phuangsombat on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
